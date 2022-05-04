edd80a5cb6fd70a5fb55816486df59f0752abea9
Vico4 <victoire.stahl@gmail.com>
2022-05-04 14:25:02 +0200

    Initial Commit

---
+++ surf/surfApp.swift
@@ -0,0 +1,17 @@
+//
+//  surfApp.swift
+//  surf
+//
+//  Created by Victoire Stahl on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct surfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
