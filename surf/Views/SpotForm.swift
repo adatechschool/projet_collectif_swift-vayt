1dcd189261f23c9f7de6b4a1cdb93dfbd249614e
Vico4 <victoire.stahl@gmail.com>
2022-05-30 10:31:08 +0200

    route post intégrée

--- surf/Views/SpotForm.swift
+++ surf/Views/SpotForm.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct SpotForm: View {
     
-    
-    struct RecordToSend: Encodable {
-        var records: [SpotFields]
-    }
-    
-    struct SpotFields : Encodable {
-        var fields: FormSpotData
-    }
+//    struct RecordToSend: Encodable {
+//        var records: [SpotFields]
+//    }
+//
+//    struct SpotFields : Encodable {
+//        var fields: FormSpotData
+//    }
     
     class FormSpotData: Encodable, ObservableObject {
         var name = ""
         var city = ""
         var country = ""
         var description = ""
-        var imageName = ""
         var latitude: Double = 0
         var longitude: Double = 0
+        var image_URL = ""
       }
     
     @ObservedObject var formData = FormSpotData()
+    
     @State private var confirmationMessage = ""
     @State private var showingConfirmation = false
     
@@ -52,7 +52,7 @@ struct SpotForm: View {
                 TextField("description", text: $formData.description)
                 TextField("latitude", value: $formData.latitude, formatter: coordFormater)
                 TextField("longitude", value: $formData.longitude, formatter: coordFormater)
-                TextField("image URL", text: $formData.imageName)
+                TextField("image URL", text: $formData.image_URL)
                 }
                 Section {
                     Button("Spook me !") {
@@ -70,14 +70,14 @@ struct SpotForm: View {
     func sendSpot() async {
         
         print("posting")
-        let dataToSend: RecordToSend = RecordToSend(records: [SpotFields(fields: formData)])
+        let dataToSend: FormSpotData =  formData
         print("posting")
         guard let encoded = try? JSONEncoder().encode(dataToSend) else {
             print("Failed to encode order")
             return
         }
 
-        let url = URL(string: "https://api.airtable.com/v0/appI8YDBcRniNVt9u/Spots?api_key=keyUf2J6tpBtwzKyG")!
+        let url = URL(string: "http://localhost:8080/spot")!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
